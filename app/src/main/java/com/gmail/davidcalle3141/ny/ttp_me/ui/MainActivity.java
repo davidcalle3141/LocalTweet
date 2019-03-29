8b02584b8340cee86131bb30a27a03f3dd114d2b
David Calle <davidcalle3141@gmail.com>
2019-03-29 13:37:02 -0400

    initial commit

---
+++ app/src/main/java/com/gmail/davidcalle3141/ny/ttp_me/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gmail.davidcalle3141.ny.ttp_me.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gmail.davidcalle3141.ny.ttp_me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
